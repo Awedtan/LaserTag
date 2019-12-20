cec41b0780f5f087a2716fb48e9e6832ef40f4a6
Su Daniel <daniel8su@outlook.com>
2019-12-20 14:48:56 -0500

    cleaned some messes caused by the merge errors

--- src/packy/player.java
+++ src/packy/player.java
@@ -37,11 +37,7 @@ public class player {
 	
 		if(sprint)
 			speed = (int)(STARTSPEED*2);
-<<<<<<< HEAD
 		else 
-=======
-		else {
->>>>>>> master
 			speed = STARTSPEED;
 		
 		if((moveLeft || moveRight) && (moveUp || moveDown))
