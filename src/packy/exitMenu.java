de81d0927c18eac4be72f57c5d33b51593cb49a1
Awedtan <31903483+Awedtan@users.noreply.github.com>
2020-01-07 10:57:47 -0500

    Menu edits (#30)
    * added states to menu, can't dynamically switch between them for now
    
    * changed around the structure of the menu
    
    Co-authored-by: agent30632 <49417970+agent30632@users.noreply.github.com>

---
+++ src/packy/exitMenu.java
@@ -0,0 +1,85 @@
+package packy;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+@SuppressWarnings("serial")
+public class exitMenu extends JPanel implements KeyListener, MouseListener{
+    JLabel confirmAsk, confirmYes, confirmNo;
+
+    public exitMenu() {
+        confirmAsk = new JLabel("Are you sure you want to quit?", SwingConstants.CENTER);
+        confirmAsk.setFont(new Font("Segoe UI", Font.PLAIN, 55));
+        confirmAsk.setBounds(menu.screenWidth / 4, 350, menu.screenWidth / 2, 120);
+        confirmAsk.setForeground(Color.WHITE);
+
+        confirmYes = new JLabel("Yes", SwingConstants.CENTER);
+        confirmYes.setFont(menu.menuFont);
+        confirmYes.setBounds(menu.screenWidth / 4 + 150, 600, 150, 120);
+        confirmYes.setForeground(Color.WHITE);
+        confirmYes.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                System.exit(0);
+            }
+        });
+
+        confirmNo = new JLabel("No", SwingConstants.CENTER);
+        confirmNo.setFont(menu.menuFont);
+        confirmNo.setBounds(3 * menu.screenWidth / 4 - 300, 600, 150, 120);
+        confirmNo.setForeground(Color.WHITE);
+        confirmNo.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                menu.menuState = menu.STATE.MAIN;
+                removeAll();
+                repaint();
+            }
+        });
+
+        add(confirmAsk);
+        add(confirmYes);
+        add(confirmNo);
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+}
--- src/packy/menu.java
+++ src/packy/menu.java
@@ -8,64 +8,120 @@ import javax.swing.*;
 @SuppressWarnings("serial")
 public class menu extends JPanel implements KeyListener, MouseListener{
 	
-	// TODO: right now this runs as an isolated menu, figure out how to integrate it with the actual game (and also add menus to that)
+	// TODO: right now this runs as an isolated menu, figure out how to integrate it with the actual game (and also add game menu to that)
 	// use my knowledge, i beg you: https://www.youtube.com/watch?v=FZWX5WoGW00
 
 	//the necessary variables
-	int screenWidth = (int) (Toolkit.getDefaultToolkit().getScreenSize().getWidth());// Screen dimensions TODO: duplicate code
-    int screenHeight = (int) (Toolkit.getDefaultToolkit().getScreenSize().getHeight());
+	static int screenWidth = (int) (Toolkit.getDefaultToolkit().getScreenSize().getWidth());// Screen dimensions
+    static int screenHeight = (int) (Toolkit.getDefaultToolkit().getScreenSize().getHeight());
 
-	Image gameLogo = Toolkit.getDefaultToolkit().getImage("images/logo_resized.png");
+	static Image gameLogo = Toolkit.getDefaultToolkit().getImage("images/logo_resized.png");
 
-	Font menuFont = new Font("Segoe UI", Font.PLAIN, 70);
-	Color menuColor = new Color(255, 255, 255);
+	static Font menuFont = new Font("Segoe UI", Font.PLAIN, 70);
 
-	// Main menu variables
-    JLabel playLabel, optionLabel, exitLabel;
+	static JFrame frame;
+
+	static mainMenu menuPanel;
+	static playMenu playPanel;
+	static exitMenu exitPanel;
 	
-	// JPanel method, throw everything in here
+	enum STATE {
+		MAIN,
+		PLAY,
+		EXIT
+	}
+	
+	static STATE menuState = STATE.MAIN;
+
+	public static void addStatePanel() {
+		// TODO: write method to add the current panel to frame based on menuState
+	}
+
+	// TODO: add every necessary thing to the other constructor files and delete this part
 	public menu() {
 		setPreferredSize(new Dimension(screenWidth, screenHeight));
 		setLayout(null);
 
-		playLabel = new JLabel("Play");
-		playLabel.setFont(menuFont);
-		playLabel.setBounds(50, 450, 250, 120);
-		playLabel.setForeground(menuColor);
-		playLabel.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				// TODO: open play menu
-			}
-		});
-		exitLabel = new JLabel("Exit");
-		exitLabel.setFont(menuFont);
-		exitLabel.setBounds(50, 700, 250, 120);
-		exitLabel.setForeground(menuColor);
-		exitLabel.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				System.exit(0);
-			}
-		});
-
-		add(playLabel);
-		add(exitLabel);
+		// Draws menu items based on the current state of the menu
+		switch (menuState) {
+			case MAIN:
+				// playLabel = new JLabel("Play");
+				// playLabel.setFont(menuFont);
+				// playLabel.setBounds(50, 450, 250, 120);
+				// playLabel.setForeground(Color.WHITE);
+				// playLabel.addMouseListener(new MouseAdapter() {
+				// 	@Override
+				// 	public void mouseClicked(MouseEvent e) {
+				// 	}
+				// });
+				// exitLabel = new JLabel("Exit");
+				// exitLabel.setFont(menuFont);
+				// exitLabel.setBounds(50, 700, 250, 120);
+				// exitLabel.setForeground(Color.WHITE);
+				// exitLabel.addMouseListener(new MouseAdapter() {
+				// 	@Override
+				// 	public void mouseClicked(MouseEvent e) {
+				// 		menuState = STATE.EXIT;
+				// 		removeAll();
+				// 		repaint();
+				// 	}
+				// });
 		
+				// add(playLabel);
+				// add(exitLabel);
+				break;
+
+			case PLAY:
+				break;
+				
+			case EXIT:
+				// confirmAsk = new JLabel("Are you sure you want to quit?", SwingConstants.CENTER);
+				// confirmAsk.setFont(new Font("Segoe UI", Font.PLAIN, 55));
+				// confirmAsk.setBounds(screenWidth / 4, 350, screenWidth / 2, 120);
+				// confirmAsk.setForeground(Color.WHITE);
+
+				// confirmYes = new JLabel("Yes", SwingConstants.CENTER);
+				// confirmYes.setFont(menuFont);
+				// confirmYes.setBounds(screenWidth / 4 + 150, 600, 150, 120);
+				// confirmYes.setForeground(Color.WHITE);
+				// confirmYes.addMouseListener(new MouseAdapter() {
+				// 	@Override
+				// 	public void mouseClicked(MouseEvent e) {
+				// 		System.exit(0);
+				// 	}
+				// });
+
+				// confirmNo = new JLabel("No", SwingConstants.CENTER);
+				// confirmNo.setFont(menuFont);
+				// confirmNo.setBounds(3 * screenWidth / 4 - 300, 600, 150, 120);
+				// confirmNo.setForeground(Color.WHITE);
+				// confirmNo.addMouseListener(new MouseAdapter() {
+				// 	@Override
+				// 	public void mouseClicked(MouseEvent e) {
+				// 		menuState = STATE.MAIN;
+				// 		removeAll();
+				// 		repaint();
+				// 	}
+				// });
+
+				// add(confirmAsk);
+				// add(confirmYes);
+				// add(confirmNo);
+				break;
+		
+			default:
+				break;
+		}	
 	}
 	
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		g.setColor(new Color(20, 20, 20));
-		g.setFont(menuFont);
 		g.fillRect(0, 0, screenWidth, screenHeight);
 
-		g.setColor(Color.WHITE);
-		g.drawImage(gameLogo, 50, 100, (2 * screenWidth/3 - 50), ((2 * screenWidth/3 - 50) / 6), this);
-		// g.drawString("Play", 50, 500);
-		// g.drawString("Options", 50, 650);
-		// // could add a credits option once everything else is done
-		// g.drawString("Exit", 50, 900);
+		if (menuState == STATE.MAIN) {
+			g.drawImage(gameLogo, 50, 100, (2 * screenWidth/3 - 50), ((2 * screenWidth/3 - 50) / 6), this);
+		}
 	}
 
 	@Override
@@ -111,17 +167,15 @@ public class menu extends JPanel implements KeyListener, MouseListener{
 	// TODO: if this can be integrated with the body.java, the main function in either this or that needs to go
 	public static void main(String[] args) {
 		//makes a brand new JFrame
-        JFrame frame = new JFrame();
+        frame = new JFrame();
         frame.setUndecorated(true);
-		menu myPanel = new menu();
-		frame.add(myPanel);
-		//so you can actually get keyboard input
-		// frame.addKeyListener(myPanel);
-		//so you can actually get mouse input
-		// frame.addMouseListener(myPanel);
+		menuPanel = new mainMenu();
+		playPanel = new playMenu();
+		exitPanel = new exitMenu();
+		frame.add(menuPanel);
 		frame.pack();
 		frame.setLocationRelativeTo(null);
         frame.setResizable(false);
-        frame.setVisible(true);
+		frame.setVisible(true);
 	}
 }
