d34161babfc37cca07e36db650004ceed8f749f0
agent30632 <49417970+agent30632@users.noreply.github.com>
2020-01-15 09:46:21 -0500

    github is weird, but hopefully my changes work
    yeet

--- src/packy/defaultValues.java
+++ src/packy/defaultValues.java
@@ -4,6 +4,8 @@ public class defaultValues {
     // The entire purpose of this file is to contain the default values we decide that the players can go back to
     // Idk why but if it works the ¯\_(ツ)_/¯
 
+    public static final int timeDM = 120;
+
     // Player defaults
     public static final boolean playerFovEnabled = true;
     public static final int playerFovRange = 50, playerViewrange = 500, playerDamage = 5, playerHealth = 50;
